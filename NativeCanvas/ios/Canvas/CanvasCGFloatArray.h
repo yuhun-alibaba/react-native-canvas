7117fa6892ec14fe1aaa2d5208d37832506ab6b1
henryluki <iamamiracle55@gmail.com>
2018-11-13 20:23:17 +0800

    copy from ios/graphic for debug

---
+++ NativeCanvas/ios/Canvas/CanvasCGFloatArray.h
@@ -0,0 +1,12 @@
+//
+//  CanvasCGFloatArray.h
+//  graphic
+//
+//  Created by sam on 2018/11/1.
+//  Copyright © 2018 sam. All rights reserved.
+//
+
+typedef struct {
+    size_t count;
+    CGFloat *array;
+} CanvasCGFloatArray;
---
+++ NativeCanvas/ios/Canvas/CanvasView.h
@@ -0,0 +1,25 @@
+//
+//  CanvasView.h
+//  graphic
+//
+//  Created by sam on 2018/11/1.
+//  Copyright © 2018 sam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CanvasRenderingContext2D.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CanvasView : UIView
+
+@property (nonatomic, strong) CanvasRenderingContext2D *context;
+@property (nonatomic, strong) NSMutableArray *actions;
+
+-(void)callAction:(NSDictionary*)methodAndArguments;
+-(void)callActions:(NSArray<NSDictionary *>*)actions;
+-(void)invalidate;
+
+@end
+
+NS_ASSUME_NONNULL_END
