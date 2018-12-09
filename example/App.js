e148b8955fef4a826ce68514572f932295e24dd0
henryluki <iamamiracle55@gmail.com>
2018-12-09 23:08:03 +0800

    js: update example

---
+++ example/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+
+import F2 from "./src/f2";
+// import Echarts from "./src/echarts";
+// import Simple from "./src/simple";
+// import PanAnimation from "./src/pan-animation";
+// import Animation from "./src/animation";
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: "Home"
+  };
+
+  render() {
+    return <Echarts />;
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen
+  },
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#fff"
+      },
+      headerTintColor: "#000",
+      headerTitleStyle: {
+        fontWeight: "bold"
+      }
+    }
+  }
+);
+
+export default createAppContainer(AppNavigator);
